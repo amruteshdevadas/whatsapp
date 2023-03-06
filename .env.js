136a3ca394c65c553a804ef0a6baf1fe345aea76
Amrutesh Devadas <85866158+amruteshdevadas@users.noreply.github.com>
2023-03-06 12:17:51 +0530

    Update .env.js

--- .env.js
+++ .env.js
@@ -8,7 +8,7 @@ const development = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     PORT: process.env.PORT || '9000',
     Meta_WA_accessToken:
-        'EAAKBQIWASUwBAK8zMx2WQqxhizCTtgsPv3qkWQ2R6Q4QZBw6pGPkDraE5wZCp0053YNyZBMmsZC2M4n8FyNZCkoIqm0ZAMJRgUjPkZCukM988mZCqZAneX0lODn8oQpXDlEDGRMjqCQNWitcAaL4D6OzCR5AZC1Dw9FKFZCNpy5JXkcn1YTk54JW9o3qZBmrJbYbM9MQIl4PyvwSf0SGI6C4ZAxdN',
+        'EAAKBQIWASUwBAICkAeiM5IaXbbpiTbfv8PB6WgBhf1ZAdm8zjeXnnrs9rM32sPJ44ySGR8uyW71P1qrGZAYYXudNFws5i8M0fDPvaczHpOeIzA6ZBsXF7ZCFQqzFsPW29rvX5VPysCOlpaPEZAxNESkZBz5rXWfgpIg1OYskMwy5naQYNmCeUEATraYZAGDfY28puheVppZCCgmwb9EE2RBk',
     Meta_WA_SenderPhoneNumberId: '101580936177175',
     Meta_WA_wabaId: '104193235911486',
     Meta_WA_VerifyToken: '12345',
